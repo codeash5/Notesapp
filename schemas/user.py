f622724b0ed305819c3358021bab55ee00bc8ba5
codeash5 <ashwinibhawalkar256@gmail.com>
2026-06-17 22:02:47 +0530

    Fixed bycrypt compatibility and password validation

--- schemas/user.py
+++ schemas/user.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 class UserCreate(BaseModel):
     email: EmailStr
-    password: str
+    password: str = Field(min_length=6, max_length=72)
 
 class UserResponse(BaseModel):
     id: int
